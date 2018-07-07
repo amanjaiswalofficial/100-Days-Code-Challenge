d24a57a6acb48f13645d120cbaf3dde5e98a4b48
Aman Jaiswal <amanjai01@gmail.com>
2018-07-07 19:12:52 +0530

    finding zeroes and put em in beginning

--- code/python/day-6/zeroahead.py
+++ code/python/day-6/zeroahead.py
@@ -4,13 +4,13 @@ arr = []
 for x in range(n):
     arr.append(int(input()))
 b=[]
-j=0
-last = len(arr)
+j=0#first index
+last = len(arr)#last index
 for i in range(len(arr)):
-    if(arr[i]==0):
+    if(arr[i]==0):#if the element is 0, then push it in the beginning
         b.insert(j,arr[i])
         j+=1
-    elif(arr[i]==1):
+    elif(arr[i]==1):#if the element is 1, then push it to the end
         b.insert(last,arr[i])
         last-=1
 print('original array: '+str(arr))

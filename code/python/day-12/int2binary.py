0f002296fbd84d8ceadcf10a758694cd050afa65
Aman Jaiswal <amanjai01@gmail.com>
2018-07-14 00:07:55 +0530

    Convert Int to binary

--- code/python/day-12/int2binary.py
+++ code/python/day-12/int2binary.py
@@ -1,7 +1,14 @@
 n=int(input('Enter Number to convert to Binary:'))
 binary=0
-while(n>1):
-    rem=n%2
-    binary=binary*10+1
-    n=n/2
-print(binary)
+a=[]
+b=[]
+while(n>0):
+    rem=int(n%2)#find out the remainder whether its 0 or 1
+    a=b.copy()#copy the already existing list of numbers
+    b=[]#empty the 2nd list
+    b.append(rem)#add the new rem in the beginning
+    b.extend(a)#append the existing list to it's end
+    n=int(n/2)
+
+for i in range(len(b)):
+    print(b[i],end="")

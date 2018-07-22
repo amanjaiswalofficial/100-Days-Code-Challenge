b9f7da2a10473bfbc32a8b91e67a5c725927fd8c
Aman Jaiswal <amanjai01@gmail.com>
2018-07-22 23:51:49 +0530

    using classes and objects

---
+++ code/python/day-21/app.py
@@ -0,0 +1,5 @@
+from student import student
+student1=student('Aman','MCA',80,True)
+print(student1.name)
+print(student1.course)
+print(student1.percent)
--- code/python/day-21/student.py
+++ code/python/day-21/student.py
@@ -1,2 +1,8 @@
 class student:
     
+    def __init__(self,name,course,percent,is_a_boy):
+        self.name=name
+        self.course=course
+        self.percent=percent
+        self.is_a_boy=is_a_boy
+    

b99606b1d793a96ca4a3f571c6c2dc0bd236c9fa
Aman Jaiswal <amanjai01@gmail.com>
2018-08-09 23:00:32 +0530

    Candy Problem

---
+++ code/python/day-39/CandyProb.py
@@ -0,0 +1,11 @@
+sums=[]
+secsum=[]
+ans=0
+inpt=[]
+n=int(input())
+for i in range(n):
+    inpt.append(int(input()))
+lim=int(input())
+for i in range(len(inpt)):
+        print(str(i+1)+': '+str(inpt[i]))
+    

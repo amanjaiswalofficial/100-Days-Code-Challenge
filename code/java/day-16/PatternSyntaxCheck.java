34f2fd882bab9e9959ab80061e3c32a3bf28d07c
Aman Jaiswal <amanjai01@gmail.com>
2018-09-01 20:33:42 +0530

    Checking pattern validity

---
+++ code/java/day-16/PatternSyntaxCheck.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class PatternSyntaxCheck {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String st="";
+        int testCases = Integer.parseInt(in.nextLine());
+        while (testCases > 0) 
+        {
+            String pattern = in.nextLine();
+            try {
+                Pattern r = Pattern.compile(pattern);
+                st=st+"Valid\n";
+            } catch (Exception e) {
+                st=st+"Invalid\n";
+            }
+            testCases--;
+        }
+        System.out.println(st);
+    }
+}

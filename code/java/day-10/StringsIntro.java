91086e06916fede7e5d365daafd5b96e3532c700
Aman Jaiswal <amanjai01@gmail.com>
2018-09-14 21:08:42 +0530

    Strings in Java

--- code/java/day-10/StringsIntro.java
+++ code/java/day-10/StringsIntro.java
@@ -8,7 +8,7 @@ public class StringsIntro
         s1=sc.nextLine();
         s2=sc.nextLine();
         System.out.println(s1.length()+s2.length());
-        if(s1.compareTo(s2)>0)
+        if(s1.compareTo(s2)>0)//comparision
             {
                 System.out.println("Yes");
             }
